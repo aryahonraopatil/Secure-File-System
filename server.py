ee869e822a40896f9908f7fa4fbbba687de22e07
Sai Krishna Vishnumolakala <saikrishna.v1970@gmail.com>
2023-11-27 22:04:03 -0500

    Update server.py

--- server.py
+++ server.py
@@ -1,35 +1,17 @@
+# server.py
 import socket
 
-# Define the server host and port
-HOST = '127.0.0.1'
-PORT = 12345
+HOST = '127.0.0.1'  # Standard loopback interface address (localhost)
+PORT = 65432        # Port to listen on (non-privileged ports are > 1023)
 
-def start_server():
-    # Create a socket object
-    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-    # Bind the socket to the host and port
-    server_socket.bind((HOST, PORT))
-
-    # Listen for incoming connections
-    server_socket.listen()
-
-    print(f"Server is listening on {HOST}:{PORT}")
-
-    while True:
-        # Accept a connection from a client
-        client_socket, client_address = server_socket.accept()
-        print(f"Accepted connection from {client_address}")
-
-        # Send a welcome message to the client
-        client_socket.sendall(b"Welcome to the server!")
-
-        # Receive data from the client
-        data = client_socket.recv(1024)
-        print(f"Received data: {data.decode('utf-8')}")
-
-        # Close the client socket
-        client_socket.close()
-
-if __name__ == "__main__":
-    start_server()
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.bind((HOST, PORT))
+    s.listen()
+    conn, addr = s.accept()
+    with conn:
+        print(f'Connected by {addr}')
+        while True:
+            data = conn.recv(1024)
+            if not data:
+                break
+            conn.sendall(data)
