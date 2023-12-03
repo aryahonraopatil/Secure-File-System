a3de18bdf8780da531f7495d616eba9b7c230ea2
arya honraopatil <ahonraopatil@gmail.com>
2023-12-03 17:27:29 -0500

    Added import functions to the central server

---
+++ code_files/central_server.py
@@ -0,0 +1,6 @@
+from PyQt5.QtWidgets import QApplication, QMainWindow, QTextEdit, QVBoxLayout, QWidget, QPushButton
+from PyQt5.QtCore import QThread, pyqtSignal
+import socket
+import threading
+import json
+import sys
