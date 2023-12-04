3f41a428ad01e0c1358604999753f3e825471a64
aramineni28 <aramineni09@gmail.com>
2023-12-03 21:10:29 -0500

    defined init method

--- code_files/peer_gui.py
+++ code_files/peer_gui.py
@@ -10,11 +10,28 @@ faulthandler.enable()
 
 
 
+
 class PeerGUI(QMainWindow):
     file_transfer_request_signal = pyqtSignal(str, int, object)
     message_received_signal = pyqtSignal(str)
 
+    def __init__(self, peer):
+        super().__init__()
+        self.peer = peer
+        self.setWindowTitle(peer.name)
+        self.initUI()
 
+        self.peer.set_message_received_callback(self.on_message_received)
+        self.message_received_signal.connect(self.display_message)
+        self.file_transfer_request_signal.connect(self.handle_file_transfer_request_gui)
+        self.peer.set_file_transfer_request_callback(lambda f, s, c: self.file_transfer_request_signal.emit(f, s, c))
+
+
+        self.peer_thread = PeerThread(self.peer)
+        self.peer_thread.update_peers_signal.connect(self.update_peers_list)
+        self.peer_thread.start()
+
+   
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
