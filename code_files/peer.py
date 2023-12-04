ef204a671592c19799f36430a2e37f488ccaa636
VenkatPantham <venkatpantham5568@gmail.com>
2023-12-03 21:56:33 -0500

    added-e/ problem accepting connections/k-check

--- code_files/peer.py
+++ code_files/peer.py
@@ -110,13 +110,16 @@ class Peer:
             self.log_message(f"Error: {e}")
 
     def check_connection(self, target_peer):
-        
-        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-            s.connect(target_peer)
-            check_message = "CONNECTION_CHECK\n"
-            s.send(check_message.encode())
-            print("Connection check message sent.")
-            self.log_message(str(f'Connection check message sent to {target_peer}'))
+        try:
+            with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+                s.connect(target_peer)
+                check_message = "CONNECTION_CHECK\n"
+                s.send(check_message.encode())
+                print("Connection check message sent.")
+                self.log_message(str(f'Connection check message sent to {target_peer}'))
+        except Exception as e:
+            print(f"Error checking connection: {e}")
+            self.log_message(str(f"Error checking connection: {e}"))
             
 if __name__ == "__main__":
     try:
