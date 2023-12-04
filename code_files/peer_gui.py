08092f3eae9bb46f8ce55fd476e5ccbc323da1e0
aramineni28 <aramineni09@gmail.com>
2023-12-03 21:07:43 -0500

    added main function

--- code_files/peer_gui.py
+++ code_files/peer_gui.py
@@ -7,3 +7,13 @@ from peer import Peer
 import faulthandler
 
 
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    peer_name = input("Enter your peer name: ")
+    try:
+        peer = Peer(peer_name)
+        gui = PeerGUI(peer)
+        gui.show()
+        sys.exit(app.exec_())
+    except Exception as e:
+        print(f"Error: {e}")
