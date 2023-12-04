cb59d867929513bcd7c43c9b51904da4a5b1759b
aramineni28 <aramineni09@gmail.com>
2023-12-03 21:06:33 -0500

    added peer_gui file and imports

---
+++ code_files/peer_gui.py
@@ -0,0 +1,9 @@
+from PyQt5.QtWidgets import QApplication, QMainWindow, QVBoxLayout, QWidget, QPushButton, QListWidget, QFileDialog, QMessageBox, QLineEdit, QTextEdit
+from PyQt5.QtCore import pyqtSignal, pyqtSlot, QThread
+import sys, time
+import threading
+from peer import Peer
+
+import faulthandler
+
+
